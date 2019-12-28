4b582d5b5631c7c5ba95ea1e02445e73adce8195
gyucode <gyucode@gmail.com>
2019-12-28 11:25:40 +0900

    로그인 화면 입력검증(Validation) 처리

---
+++ app/src/main/java/com/example/a20191228_01_apipractice/LoginActivity.kt
@@ -0,0 +1,44 @@
+package com.example.a20191228_01_apipractice
+
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setupEvents()
+        setValue()
+    }
+
+    override fun setupEvents() {
+//        로그인 버튼이 눌리면
+//        1. ID 입력이 빈칸이라면 "ID를 입력해주세요." 토스트 출력
+//        2. PW 입력이 8글자 미만이라면 "비번이 너무 짧습니다" 토스트 출력
+//        3. 둘다 괜찮다면 별개로 분기만 준비.
+
+        loginBtn.setOnClickListener {
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+            if(inputId == ""){
+                Toast.makeText(mContext, "ID를 입력해주세요",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(inputPw.length < 8 ){
+                Toast.makeText(mContext, "비번이 너무 짧습니다",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+//        여기의 코드는 ID/ PW가 모두 정상적이여야만 실행 가능
+            Toast.makeText(mContext, "정상 입력이라 로그인",Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+    override fun setValue() {
+
+    }
+
+}
