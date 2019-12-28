4e12087949bca054d33e2eca12ec21b29f9ea41c
gyucode <gyucode@gmail.com>
2019-12-28 16:58:24 +0900

    연습문제 준비  - 사용자 목록에서 궁금한 사람을 누르면  - UserInfoActivity로 넘어와서  - 해당 사용자의 정보를 텍스트뷰로 출력

---
+++ app/src/main/java/com/example/a20191228_01_apipractice/UserInfoActivity.kt
@@ -0,0 +1,21 @@
+package com.example.a20191228_01_apipractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserInfoActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info)
+        setupEvents()
+        setValue()
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValue() {
+
+    }
+
+}
